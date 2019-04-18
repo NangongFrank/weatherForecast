88c322487790531780e1214a3ee70ccc9048494e
zhao mac <3345@qq.com>
2019-04-18 09:01:50 +0800

    change sql

--- wf_php/wf_db.sql
+++ wf_php/wf_db.sql
@@ -2641,7 +2641,7 @@ INSERT INTO `t_user` (`id`, `name`, `phone`, `pwd`, `last_login`, `u_type`) VALU
 
 CREATE TABLE `u_side` (
   `id` int(11) NOT NULL,
-  `phone` char(11) NOT NULL,
+  `u_id` varchar(20) NOT NULL,
   `code` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
