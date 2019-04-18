d0eac1f254aab0d18b54e3085c01d224834516c8
zhao mac <3345@qq.com>
2019-04-18 22:23:05 +0800

    change db

--- wf_php/models/admin.class.php
+++ wf_php/models/admin.class.php
@@ -2,14 +2,14 @@
 /**
  * @Author: name
  * @Date:   2019-03-20 16:24:32
- * @Last Modified by:   name
- * @Last Modified time: 2019-04-17 16:16:57
+ * @Last Modified by:   zhao mac
+ * @Last Modified time: 2019-04-18 22:22:49
  */
 class Admin extends DBModel {
     function getadmin($options) {
         $code = $options["code"];
         $pwd = $options["pwd"];
-        $sql = "SELECT id as code, phone, name from t_user where phone = '$code' or name = '$code' and pwd = md5('$pwd') and id = 1";
+        $sql = "SELECT id as code, phone, name from t_user where (phone = '$code' or name = '$code') and pwd = md5('$pwd') and id = 1";
         $up = "UPDATE t_user set last_login = now() where (phone = '$code' or name = '$code') and pwd = md5('$pwd')";
         $res = $this -> getrow($sql);
         $this -> exec($up);
