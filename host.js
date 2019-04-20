dd89c30eb4bf679881810110628a127055488b0e
zhao mac <3345@qq.com>
2019-04-20 22:43:24 +0800

    update

--- host.js
+++ host.js
@@ -3,6 +3,8 @@ export var sideAPI = "http://api.map.baidu.com/geocoder/v2/"
 // 天气预报API使用魅族天气
 export var weatherAPI = "http://aider.meizu.com/app/weather/listWeather"
 // 天气预警提示API
-//export var myServer = "http://120.79.228.56/wf_php/index.php"
+
+// 资源服务器
+export var myServer = "http://120.79.228.56/wf_php/index.php"
 //export var myServer = "http://192.168.1.195/wf_php/index.php"
-export var myServer = "http://localhost/wf_php/index.php"
+//export var myServer = "http://localhost/wf_php/index.php"
