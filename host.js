28ffe42d00c27b95f12d971294d5b8c6ea90416a
zhao mac <3345@qq.com>
2019-07-02 22:22:33 +0800

    update change host

--- host.js
+++ host.js
@@ -1,10 +1,15 @@
 // 百度Web服务API
-export var sideAPI = "http://api.map.baidu.com/geocoder/v2/"
+let sideAPI = "http://api.map.baidu.com/geocoder/v2/"
 // 天气预报API使用魅族天气
-export var weatherAPI = "http://aider.meizu.com/app/weather/listWeather"
+let weatherAPI = "http://aider.meizu.com/app/weather/listWeather"
 // 天气预警提示API
 
+
+
 // 资源服务器
-export var myServer = "http://120.79.228.56/wf_php/index.php"
+//let myServer = "http://120.79.228.56/wf_php/index.php"
+let myServer = "https://www.ngfrankgl.cn/wf_php/index.php"
 //export var myServer = "http://192.168.1.195/wf_php/index.php"
 //export var myServer = "http://localhost/wf_php/index.php"
+
+export {sideAPI, weatherAPI, myServer}
